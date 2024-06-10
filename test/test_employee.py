8b269cb3e8be006b847c4aac5413b06e8272659f
leonid_popcorn <160226209+LeonidPopcornHSE@users.noreply.github.com>
2024-06-10 11:55:55 +0300

    Add files via upload

---
+++ test/test_employee.py
@@ -0,0 +1,19 @@
+import pytest
+
+from main.employee import Employee
+def test_create_employee():
+    employee = Employee("Иван", "Менеджер", 6000)
+    assert employee.name == "Иван"
+    assert employee.position == "Менеджер"
+    assert employee.salary == 6000
+
+def test_create_employee_fail():
+    with pytest.raises(ValueError):
+        Employee("Иван", "Менеджер", "6000")
+
+def test_count_employee():
+    employee1 = Employee("Андрей", "Инженер", 2000)
+    employee2 = Employee("Мария", "Менеджер",5000)
+    assert Employee.emp_count == 2
+    employee3 = Employee("Юлия", "Переводчик", 800)
+    assert Employee.emp_count == 3
