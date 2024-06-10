a3fe0266ba8d6fdfcac64e07573e1b75243f5f9c
leonid_popcorn <160226209+LeonidPopcornHSE@users.noreply.github.com>
2024-06-10 12:44:33 +0300

    Update employee.py

--- main/employee.py
+++ main/employee.py
@@ -1,5 +1,7 @@
 class Employee:
     emp_count = 0
+    projects = {}
+    # add projects 
 
     def __init__(self, name, position, salary):
         self.name = name
@@ -15,3 +17,20 @@ class Employee:
 
     def display_employee(self):
         print('Имя: {}. Зарплата: {}'.format(self.name, self.salary))
+
+    def add_employee_to_project(self, name_project):
+        if name_project not in Employee.projects:
+            Employee.projects[name_project] = set([self.name])
+        else:
+            Employee.projects[name_project].add(self.name)
+
+    @classmethod
+    def info_projects(cls):
+        print(f'Количество проектов: {len(Employee.projects)}')
+        print("Информация о каждом из проектов:")
+        i = 1
+        for key in Employee.projects.keys():
+            print(f"    {i}. Название: {key}")
+            for participant in Employee.projects[key]:
+                print("         ", participant)
+            i += 1
