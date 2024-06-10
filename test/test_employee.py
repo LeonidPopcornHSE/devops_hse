4826729fa89152e0a70b622ecc3640f97f33a03b
leonid_popcorn <160226209+LeonidPopcornHSE@users.noreply.github.com>
2024-06-10 12:04:44 +0300

    Update test_employee.py

--- test/test_employee.py
+++ test/test_employee.py
@@ -2,16 +2,18 @@ import pytest
 
 from main.employee import Employee
 def test_create_employee():
-    employee = Employee("Иван", "Менеджер", 6000)
-    assert employee.name == "Иван"
-    assert employee.position == "Менеджер"
-    assert employee.salary == 6000
+    employee1 = Employee("Иван", "Менеджер", 6000)
+    assert employee1.name == "Иван"
+    assert employee1.position == "Менеджер"
+    assert employee1.salary == 6000
 
 def test_create_employee_fail():
     with pytest.raises(ValueError):
         Employee("Иван", "Менеджер", "6000")
 
 def test_count_employee():
-    employee1 = Employee("Андрей", "Инженер", 2000)
-    employee2 = Employee("Мария", "Менеджер",5000)
-    assert Employee.emp_count == 2
+    employee2 = Employee("Андрей", "Инженер", 2000)
+    employee3 = Employee("Мария", "Менеджер",5000)
+    assert Employee.emp_count == 3
+    employee4 = Employee("Юлия", "Переводчик", 800)
+    assert Employee.emp_count == 4
